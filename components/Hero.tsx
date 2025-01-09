8c411dbfa08af5a742667b8a865a18f460c2ae8f
zafar-m3n <thezafar.m3n@gmail.com>
2025-01-09 17:49:16 +0530

    Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,14 +3,15 @@ import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import { MagicButton } from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
+import { MdAllInclusive } from "react-icons/md";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
       <div>
         <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
-        <Spotlight className="top-40 left-full h-[80vh] w-[50vw]" fill="green" />
-        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="green" />
+        <Spotlight className="top-40 left-full h-[80vh] w-[50vw]" fill="#0bf40a" />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="#eff00f" />
       </div>
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
@@ -18,15 +19,19 @@ const Hero = () => {
       </div>
       <div className="flex justify-center relative z-10 my-20">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-center text-green-100 max-w-80">
-            Dynamic Web Magic with Next.js
+          <h2 className="uppercase tracking-widest text-xs text-center text-green-100 flex items-center space-x-4 max-w-lg">
+            <span>Our Capital, Your</span>
+            <span className="flex items-center space-x-2 bg-white/20 backdrop-blur-md border border-white/30 rounded-full px-2 py-1 shadow-md">
+              <MdAllInclusive size={16} color="#0bf40a" />
+              <p className="text-white font-medium">Success</p>
+            </span>
           </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless Experiences"
+            words="Your Journey to Financial Freedom"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi, I'm Zafarullah, a fullstack developer from Sri Lanka.
+            Achieve your goals with a platform designed for traders of all levels.
           </p>
           <a href="#">
             <MagicButton title="Get Started" icon={<FaLocationArrow />} position="right" />
